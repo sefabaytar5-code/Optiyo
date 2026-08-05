e76e5a9e953d5f6e59767e9a0eb080090f01a99b
sefabaytar5-code <sefabaytar5@gmail.com>
2026-08-05 23:59:11 +0300

    Add Deal model with properties and constructor

--- lib/models/deal_model.dart
+++ lib/models/deal_model.dart
@@ -1,1 +1,17 @@
+class Deal {
+  final String id;
+  final String title;
+  final String description;
+  final String? discountCode;
+  final String category;
+  final bool isHidden;
 
+  Deal({
+    required this.id,
+    required this.title,
+    required this.description,
+    this.discountCode,
+    required this.category,
+    this.isHidden = false,
+  });
+}
